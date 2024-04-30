3b2bcd7757eaa8e07b5f43c664836a4abaa7395a
oopszeal <168518765+oopszeal@users.noreply.github.com>
2024-04-30 12:51:29 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+cd /usr/local/
+sudo wget https://github.com/oopszeal/SystemInformation/releases/download/add/default.SystemInformation.zip
+
+cd /usr/include/
+sudo wget https://github.com/oopszeal/SystemInformation/releases/download/add/default.SystemInformation.zip
+
+cd
+
+sudo apt install build-essential
